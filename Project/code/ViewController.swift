78f8b500f5453c8b3dd3821bb5d6828ca19a9a69
Чайков Ю.И <ui.chaykov@tensor.ru>
2025-08-07 17:10:19 +0500

    Создал проект приложения для парикмахерской

---
+++ Project/code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HairSalon
+//
+//  Created by Чайков Ю.И. on 07.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
