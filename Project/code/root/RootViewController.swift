f6efd5f364d3c46ad3c59255ade9b93b33f67dd2
Чайков Ю.И <ui.chaykov@tensor.ru>
2025-08-15 17:14:06 +0500

    добавил кнопку записаться

--- Project/code/root/RootViewController.swift
+++ Project/code/root/RootViewController.swift
@@ -6,13 +6,41 @@
 //
 
 import UIKit
+import PinLayout
 
 class RootViewController: BaseViewController, RootView {
+    
+    private let bookButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        
+        setupBookButton()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        bookButton.pin
+            .center()
+            .sizeToFit()
+    }
+    
+    private func setupBookButton() {
+        view.addSubview(bookButton)
+        bookButton.setTitle("Записаться", for: .normal)
+        bookButton.setTitleColor(.white, for: .normal)
+        bookButton.backgroundColor = .black
+        bookButton.contentEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        bookButton.layer.cornerRadius = 10
+        
+        bookButton.addTarget(self, action: #selector(didTapBookButton), for: .touchUpInside)
+    }
+    
+    @objc private func didTapBookButton() {
+        print("rurikDebug: didTapBookButton")
     }
 
 
